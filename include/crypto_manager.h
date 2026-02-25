7e4dafc4e334fbfcccbb517f6e3f7b9703d860b7
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-02-25 13:46:54 +0700

    feat: add CryptoManager singleton class
    Добавлен заголовочный файл класса CryptoManager

---
+++ include/crypto_manager.h
@@ -0,0 +1,40 @@
+﻿#ifndef CRYPTO_MANAGER_H
+#define CRYPTO_MANAGER_H
+
+#include <QString>
+
+class CryptoManager final
+{
+public:
+    // Получение единственного экземпляра
+    static CryptoManager &instance();
+
+    // Шифрование всех файлов в папке и подпапках
+    bool encryptFolder(const QString &folderPath, const QString &password);
+
+    // Дешифрование всех файлов в папке и подпапках
+    bool decryptFolder(const QString &folderPath, const QString &password);
+
+private:
+    // Конструктор
+    CryptoManager() = default;
+    // Деструктор
+    ~CryptoManager() = default;
+
+    // Запрет копирования и присвоения
+    CryptoManager(const CryptoManager &) = delete;
+    CryptoManager &operator=(const CryptoManager &) = delete;
+
+    // Шифрование одного файла
+    bool encryptFile(const QString &filePath, const QString &password);
+    // Дешифрование одного файла
+    bool decryptFile(const QString &filePath, const QString &password);
+
+    // Проверка, был ли файл зашифрован нашей программой
+    bool isEncryptedFile(const QString &filePath) const;
+
+    // Проверка существования и корректности директории
+    static bool isValidDirectory(const QString &path);
+};
+
+#endif // CRYPTO_MANAGER_H
