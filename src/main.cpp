b1abe69cd947eaee0ff555f034607f86d9c6639b
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-03-10 13:27:17 +0700

    Перемещение main.cpp в /src

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <QCoreApplication>
+#include <QTextStream>
+
+#include "crypto_manager.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    QTextStream in(stdin);
+    QTextStream out(stdout);
+    QTextStream err(stderr);
+
+    const QStringList args = app.arguments();
+
+    if (args.size() != 3) {
+        out << "Usage:\n"
+            << "  " << args.value(0) << " <folder_path> <password>\n";
+        return 1;
+    }
+
+    const QString folderPath = args.at(1);
+    const QString password = args.at(2);
+
+    out << "==== Data Protection System ====\n";
+    out << "1 - Encrypt folder\n";
+    out << "2 - Decrypt folder\n";
+    out << "Select option: ";
+    out.flush();
+
+    const QString choice = in.readLine().trimmed();
+
+    bool ok = false;
+
+    if (choice == "1") {
+        ok = CryptoManager::instance().encryptFolder(folderPath, password);
+    } else if (choice == "2") {
+        ok = CryptoManager::instance().decryptFolder(folderPath, password);
+    } else {
+        err << "Invalid option.\n";
+        return 2;
+    }
+
+    out << (ok ? "Done.\n" : "Done with errors.\n");
+    return ok ? 0 : 3;
+}
