9c4ee177df2283d3e766df7d200a46677117606b
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-03-20 13:54:09 +0700

    feat: add the FrequencyRefresher class for periodic updates files info

--- include/IFileRefresher.h
+++ include/IFileRefresher.h
@@ -1,11 +1,37 @@
 ﻿#ifndef IFILEREFRESHER_H
 #define IFILEREFRESHER_H
 
+#include <chrono>
+#include <thread>
+#include <time.h>
 class IFileRefresher
 {
 public:
     virtual ~IFileRefresher() = default;
     virtual void refresh() = 0;
 };
+
+class FrequencyRefresher : public IFileRefresher
+{
+  private:
+    float _refreshRate = 1000.0f; // Частота в Гц (раз в секунду)
+
+  public:
+    explicit FrequencyRefresher(float refreshRate)
+        : _refreshRate(refreshRate)
+    {
+        // Защита от деления на ноль и отрицательных значений
+        if (_refreshRate <= 0.0f) {
+            _refreshRate = 1.0f; 
+        }
+    }
+
+    void refresh() override {
+        // Вычисляем задержку в миллисекундах: 1000 мс / частота
+        auto ms = static_cast<int>(1000.0f / _refreshRate);
+        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    }
+};
+
 
 #endif // IFILEREFRESHER_H
