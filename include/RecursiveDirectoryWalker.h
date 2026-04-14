87d2e9b7726619625b97d9e7b870f18ad9942fe2
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-04-14 13:57:50 +0700

    refactor: extract RecursiveDirectoryWalker declaration to RecursiveDirectoryWalker.h

---
+++ include/RecursiveDirectoryWalker.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "IDirectoryWalker.h"
+
+class RecursiveDirectoryWalker : public IDirectoryWalker {
+public:
+    QStringList getFiles(const QString &rootDir) const override;
+};
--- src/RecursiveDirectoryWalker.cpp
+++ src/RecursiveDirectoryWalker.cpp
@@ -1,12 +1,9 @@
-#include "IDirectoryWalker.h"
+﻿#include "RecursiveDirectoryWalker.h"
 #include <QDirIterator>
 
-class RecursiveDirectoryWalker : public IDirectoryWalker {
-public:
-    QStringList getFiles(const QString& rootDir) const override {
-        QStringList files;
-        QDirIterator it(rootDir, QDir::Files | QDir::NoSymLinks | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
-        while (it.hasNext()) files << it.next();
-        return files;
-    }
-};
+QStringList RecursiveDirectoryWalker::getFiles(const QString &rootDir) const {
+    QStringList files;
+    QDirIterator it(rootDir, QDir::Files | QDir::NoSymLinks | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
+    while (it.hasNext()) files << it.next();
+    return files;
+}
