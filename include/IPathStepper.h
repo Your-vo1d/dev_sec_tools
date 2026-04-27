64334bfa6f4c53ea2603fa2cfe63face22f4fa58
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-04-28 01:59:10 +0700

    Add directory traversal interface and recursive implementation

---
+++ include/IPathStepper.h
@@ -0,0 +1,15 @@
+﻿#ifndef IPATHSTEPPER_H
+#define IPATHSTEPPER_H
+
+#include <QString>
+#include <vector>
+
+
+class IPathStepper {
+public:
+    virtual ~IPathStepper() = default;
+    virtual std::vector<QString> collectFiles(const QString& root) const = 0;
+};
+
+
+#endif // IPATHSTEPPER_H
---
+++ src/RecursivePathStepper.cpp
@@ -0,0 +1,16 @@
+﻿#include "RecursivePathStepper.h"
+#include <QDir>
+
+std::vector<QString> RecursivePathStepper::collectFiles(const QString& root) const {
+    std::vector<QString> files;
+    QDir dir(root);
+    if (!dir.exists()) return files;
+
+    // Рекурсивный обход, возвращаем только файлы
+    QDirIterator it(root, QDir::Files, QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        it.next();
+        files.push_back(it.filePath());
+    }
+    return files;
+}
