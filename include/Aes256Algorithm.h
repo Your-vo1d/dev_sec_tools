d924086eaa99a3c9250dfc084dc1878f95717842
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-04-28 00:46:51 +0700

    feat: add interface for algorithms for encrypt, decrypt files, AES algorithm

---
+++ include/Aes256Algorithm.h
@@ -0,0 +1,26 @@
+﻿#ifndef AES256ALGORITHM_H
+#define AES256ALGORITHM_H
+
+#include "ICryptoAlgorithm.h"
+#include "CryptoConfig.h"
+#include <QByteArray>
+#include <QFile>
+#include <cryptopp/aes.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/osrng.h>
+
+
+class Aes256Algorithm : public ICryptoAlgorithm {
+public:
+    QString name() const override;
+    bool encrypt(const QString& path, const QString& password) override;
+    bool decrypt(const QString& path, const QString& password) override;
+    bool isEncryptedFile(const QString& path) const override;
+
+private:
+    QByteArray computeHash(const QString& password) const;
+};
+
+#endif // AES256ALGORITHM_H
---
+++ include/ICryptoAlgorithm.h
@@ -0,0 +1,17 @@
+﻿#ifndef ICRYPTOALGORITHM_H
+#define ICRYPTOALGORITHM_H
+
+#include <QString>
+
+
+class ICryptoAlgorithm {
+public:
+    virtual ~ICryptoAlgorithm() = default;
+    virtual bool encrypt(const QString& path, const QString& password) = 0;
+    virtual bool decrypt(const QString& path, const QString& password) = 0;
+    virtual bool isEncryptedFile(const QString& path) const = 0;
+    virtual QString name() const = 0;
+};
+
+
+#endif // ICRYPTOALGORITHM_H
