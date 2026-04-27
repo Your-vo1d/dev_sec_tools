f15282525ec7c4e0d4f73f439d2e062b012501b0
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-04-28 01:18:59 +0700

    feat: add logger interface and console logger implementation

---
+++ src/ConsoleLogger.cpp
@@ -0,0 +1,14 @@
+﻿#include "ConsoleLogger.h"
+#include <QTextStream>
+#include <cstdio>
+
+
+void ConsoleLogger::info(const QString& msg) {
+    QTextStream out(stdout);
+    out << "[INFO] " << msg << Qt::endl;
+}
+
+void ConsoleLogger::error(const QString& msg) {
+    QTextStream err(stderr);
+    err << "[ERROR] " << msg << Qt::endl;
+}
